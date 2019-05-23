87f0234554fb918bd4f1346161657ac39a54bfbc
Martin Sabkov <martin.sabkov@gmail.com>
2019-05-23 09:33:51 +0200

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -73,6 +73,7 @@ function initMap() {
 
 setInterval(
 function () {
+    axios.get('http://localhost/server/Laravel_Final_Project/laravel_planes/public/planes/')
 
         .then(function (response) {
             var infowindow = new google.maps.InfoWindow();
@@ -133,6 +134,7 @@ function () {
 
                 google.maps.event.addListener(planeIcon, 'click', (function (planeIcon, i) {
                     return function () {
+                        axios.get('http://localhost/server/Laravel_Final_Project/laravel_planes/public/planes/' + response.data["states"][i][0].toLowerCase())
                             .then(function (res) {
                                        /* var div = document.createElement("div");
                                         div.className += "obj";
